f52b2bf12c682c6b9fa2c0dc6bebd20fd3b0f979
L D <tapircode@gmail.com>
2019-04-10 04:49:43 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(c)
- 
+  if block_given?
+    i = 0
+    result = []
+    while i < c.length
+      value = yield c[i]
+      result << value
+      i += 1
+    end
+    return result
+  else
+    i = 0
+    while i < c.length
+      yield c[i]
+      i += 1
+    end
+    return c
+  end
 end
